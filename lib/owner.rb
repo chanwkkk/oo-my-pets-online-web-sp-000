9b97588074c8735eea96a80793b1efd1fda31c34
Yingqi Chen <chanwk18@hotmail.com>
2019-04-12 19:43:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
   # code goes here
+  attr_accessor :name
+  attr_writer :pets
+  @@all=[]
+
+  def initialize(species)
+    @species=species
+     @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all=[]
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def pets
+    {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def buy_fish(name)
+     pets[:fishes]<<Fish.new(name)
+  end
+
+
 end
